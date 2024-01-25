4128c87d9d7be944ccdc06499f5cb19838511e6c
EdgarSalazarH <155699118+EdgarSalazarH@users.noreply.github.com>
2024-01-24 19:43:51 -0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',function(){console.log("Comota Muchachos");
+      var inputEmail = document.getElementbyId('email');
+      var inputPassword = document.getElementbyId('Password');
+      console.log(inputEmail);
+      console.log(inputPassword);
+});
