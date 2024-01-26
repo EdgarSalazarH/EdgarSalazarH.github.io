aeb774c898760f821e352d499faf4ea0b930c353
EdgarSalazarH <155699118+EdgarSalazarH@users.noreply.github.com>
2024-01-25 21:03:53 -0600

    Update Email.js

--- Email.js
+++ Email.js
@@ -1,16 +1,1 @@
-function validateEmail() {
-    // Datos de entrada
-    var emailField = document.getElementById('email');
 
-    // Expresiones regulares
-    var validEmail = /^\w+([.-_+]?\w+)@\w+([.-]?\w+)(\.\w{2,10})+$/;
-
-    // Textos de Validación
-    if (validEmail.test(emailField.value)) {
-        alert('Email válido. ¡Bienvenido!');
-        return true;
-    } else {
-        alert('Email inválido. Email incorrecto.');
-        return false;
-    }
-}
